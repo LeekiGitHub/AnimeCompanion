0d7bb7bb6f2d971dc0ebc3cfd2bdbbde47ea8315
LeekiGitHub <yannikleekes@yahoo.de>
2025-12-31 15:25:16 +0100

    Initial Commit

---
+++ AnimeCompanion/AnimeCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeCompanionApp.swift
+//  AnimeCompanion
+//
+//  Created by Yannik Luca Leekes on 31.12.25.
+//
+
+import SwiftUI
+
+@main
+struct AnimeCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
